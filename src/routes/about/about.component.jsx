b4bed9fcbdbcbab74629e2008675c9a46b3ca114
Simone Carriero <simonecarriero95@hotmail.com>
2022-07-17 15:15:56 +0200

    Created about component

---
+++ src/routes/about/about.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './about.style.scss'
+
+const About = () => {
+  return (
+      <div className='about'>
+          <div className='about-background'></div>
+          <div className='about-text-container'>
+              <h2>About Us</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quas ipsa illum explicabo reiciendis ratione veritatis. Molestias nesciunt, assumenda asperiores necessitatibus incidunt facilis explicabo harum. Dicta illum ducimus fuga voluptatem!
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur temporibus ad, perspiciatis ducimus veritatis autem iure ullam, sunt perferendis delectus, eligendi vero repudiandae quaerat esse? Amet impedit est veritatis, optio, earum quidem tempora doloribus eos quam molestiae dicta minima minus incidunt unde corporis eum perferendis iure sed! Illum, aliquid quidem!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, facere?
+            </p>
+      </div>
+      </div>
+  )
+}
+
+export default About
