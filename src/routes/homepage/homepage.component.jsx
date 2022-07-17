6876f66bea89cad8a1ce150b193d0697b41c6bcf
Simone Carriero <simonecarriero95@hotmail.com>
2022-07-17 14:57:49 +0200

    Created homepage

---
+++ src/routes/homepage/homepage.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './homepage.style.scss'
+
+const Homepage = () => {
+    return (
+      
+      <div className="homepage">
+          <div className='homepage-text-container'>
+              <h1>Food Corner</h1>
+              <h2>JAPANESE FOOD AT A CLICK</h2>
+              <button>ORDER NOW</button>
+          </div>
+      </div>
+  )
+}
+
+export default Homepage
